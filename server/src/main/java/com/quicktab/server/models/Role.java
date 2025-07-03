aeef29531f24b30bbb33ec8a35c444e7c43a4bc6
siddhant-afk <siddhantdudhane21@gmail.com>
2025-07-03 10:31:37 +0530

    Add Role enum and update User model

---
+++ server/src/main/java/com/quicktab/server/models/Role.java
@@ -0,0 +1,6 @@
+package com.quicktab.server.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- server/src/main/java/com/quicktab/server/models/User.java
+++ server/src/main/java/com/quicktab/server/models/User.java
@@ -4,6 +4,8 @@ package com.quicktab.server.models;
 import jakarta.persistence.*;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -13,18 +15,87 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    private String name;
+    @Column(unique = true, nullable = false)
     private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Product> products = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Customer> customers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Invoice> invoices = new ArrayList<>();
 
     public User() {
     }
 
-    public User(Long id, String email) {
-        this.id = id;
+    public User(String name, String email, String password, Role role) {
+        this.name = name;
         this.email = email;
+        this.password = password;
+        this.role = role;
     }
 
     public Long getId() {
         return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public List<Invoice> getInvoices() {
+        return invoices;
+    }
+
+    public void setInvoices(List<Invoice> invoices) {
+        this.invoices = invoices;
     }
 
     public void setId(Long id) {
